1c6c5ddc65f48ce13aa3c9cb442e1cc27c400f65
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-10-17 15:49:09 -0500

    updating test to have the correct script.

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,4 +1,20 @@
-const { TestWatcher } = require("@jest/core");
+const Intern = require("../lib/Intern");
 
-//test change of role
-TestWatcher('test to see if the role changes to intern')
+ describe("getRole", () => {
+   describe("Initialization", () => {
+      it("should return the corrent role for the intern", () => {
+//       const obj = new Arithmetic();
+      const intern = new Intern('bob','53','bob@email.com','Employee','UT');
+//       expect("number" in obj).toEqual(true);
+      intern.getRole();
+      expect(intern.role).toBe("Intern");
+    });
+   });
+});
+
+// test('Test to see if role is changed', function() {
+//      expect(intern.getRole().toBe("Intern"));
+//  });
+//  test('test adding two positive nums', function() {
+//     expect(testFns.testForAdd(4, 5)).toBe(9);
+//  });
