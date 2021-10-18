3b9a607b0f90de0506b9eb1f0e607b4186065c2b
DerTodd <88950531+DerTodd@users.noreply.github.com>
2021-10-17 19:05:54 -0500

    CSS updated to look better.

--- index.js
+++ index.js
@@ -5,37 +5,45 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const endHTML =
-`</main>
+`
+</div>
+</div>
+</main>
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous">
+</script>
 </html>
 `;
 const enamendHTML = (engineer) =>
 `
+<div class="card cols clr">
     <div class="engineers">
-    <div id="${engineer.name}" class="container">
-    <h2 class="en_name display-4">Engineer</h2>
-        <p class="lead">Engineer</p>
-        <h3>${engineer.name}<span class="badge badge-secondary">Engineer</span></h3>
-        <ul class="list-group">
-        <li class="list-group-item">ID ${engineer.id}</li>
-        <li class="list-group-item">Email: ${engineer.email}</li>
-        <li class="list-group-item">Github Information: ${engineer.github}</li>
-        </ul>
+        <div id="${engineer.name}" class="container">
+        <h2 class="en_name display-4">${engineer.name}</h2>
+            <h3><span class="badge badge-secondary">Engineer</span></h3>
+            <ul class="list-group">
+            <li class="list-group-item">ID ${engineer.id}</li>
+            <li class="list-group-item"><a href="mailto:${engineer.email}">Email: ${engineer.name}</a></li>
+            <li class="list-group-item"><a href="https://github.com/${engineer.github}" target="_blank" rel="noopener noreferrer">${engineer.github}</a></li>
+            </ul>
+        </div>
     </div>
-    </div>
+</div>    
 `;
 const inamendHTML = (intern) =>
 `
-<div class="interns">
-    <div id="${intern.name}" class="container">
-        <h2 class="in_name display-4">Intern</h2>
-        <p class="lead">Intern</p>
-        <h3>${intern.name} <span class="badge badge-secondary">Intern</span></h3>
-        <ul class="list-group">
-        <li class="list-group-item">ID ${intern.id}</li>
-        <li class="list-group-item">Email: ${intern.email}</li>
-        <li class="list-group-item">School: ${intern.school}</li>
-        </ul>
+<div class="card cols clr">
+    <div class="interns">
+        <div id="${intern.name}" class="container">
+            <h2 class="in_name display-4">${intern.name}</h2>
+            <h2><span class="badge badge-secondary">Intern</span></h2>
+            <ul class="list-group">
+            <li class="list-group-item">ID ${intern.id}</li>
+            <li class="list-group-item"><a href="mailto:${intern.email}">Email: ${intern.name}</a></li>
+            <li class="list-group-item">School: ${intern.school}</li>
+            </ul>
+        </div>
     </div>
 </div>
 `;
@@ -45,26 +53,35 @@ const generateHTML = (manager) =>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href='./style.css'>
     <title>Bad Company</title>
 </head>
 <body>
-    <div class="topbc jumbotron jumbotron-fluid">
+    <div class="jumbotron jumbotron-fluid blue cursive">
+        <h1>Bad Company</h1>
         <banner>It's Fine.  Everything's Fine</banner>
     </div>
-    <main>    
-    <div id="manager"class="container">
-        <h2 class="display-4">${manager.name}</h2>
-        <p class="lead">Manager</p>
-        <div>
-            <ul class="list-group">
-                <li class="list-group-item">ID ${manager.id}</li>
-                <li class="list-group-item">Email: ${manager.email}</li>
-                <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
-            </ul>
-        </div>    
-    </div>
+    <main> 
+    <div class="content flow">
+    <div class="grid-ish">
+        <div class="card cols clr">   
+            <div id="manager"class="container">
+                <h2 class="display-4">${manager.name}</h2>
+                <h2><span class="badge badge-secondary">Manager</span></h2>
+                <div>
+                    <ul class="list-group">
+                        <li class="list-group-item">ID ${manager.id}</li>
+                        <li class="list-group-item"><a href="mailto:${manager.email}">Email: ${manager.name}</a></li>
+                        <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+                    </ul>
+                </div>    
+            </div>
+        </div>
     `;
 
 //const htmlEnContent = enamendHTML
@@ -80,12 +97,12 @@ const managerQuestions = [
     {
     type: 'input',
     name: 'id',
-    message: "Employee's ID number:",
+    message: "Manager's ID number:",
     },
     {
     type: 'input',
     name: 'email',
-    message: "Employee's email",
+    message: "Manager's email",
     },
     {
     type: 'input',
@@ -109,12 +126,12 @@ const engineerQuestions = [
     {
     type: 'input',
     name: 'id',
-    message: "Employee's ID number:",
+    message: "Engineer's ID number:",
     },
     {
     type: 'input',
     name: 'email',
-    message: "Employee's email",
+    message: "Engineer's email",
     },
     {
     type: 'rawlist',
@@ -125,7 +142,7 @@ const engineerQuestions = [
     {
     type: 'input',
     name: 'github',
-    message: "Employee's Github name:",
+    message: "Engineer's Github name:",
     },
 ];
 const internQuestions = [
@@ -137,12 +154,12 @@ const internQuestions = [
     {
     type: 'input',
     name: 'id',
-    message: "Employee's ID number:",
+    message: "Intern's ID number:",
     },
     {
     type: 'input',
     name: 'email',
-    message: "Employee's email",
+    message: "Intern's email",
     },
     {
     type: 'rawlist',
@@ -161,7 +178,7 @@ const internOrEngineer =[
     type: 'rawlist',
     name: 'enOrIn',
     message: 'Whould you like to add another employee?',
-    choices: ['Engineer', 'Intern', 'Build Site']
+    choices: ['Engineer', 'Intern', 'Build Website']
     },
 ];
 
